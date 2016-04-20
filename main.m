ee882eaf4791386ec5dc0984cf03dbc023fad38f
Long Le <longle1@illinois.edu>
2016-04-20 14:08:04 -0500

    added power, spl

--- main.m
+++ main.m
@@ -22,39 +22,58 @@ EVENT = 'event';
 %% Main
 
 % Query limited files from the database
-q.t1 = datenum(2016,04,19,00,00,00); q.t2 = datenum(2016,04,22,00,00,00);
+q.t1 = datenum(2016,04,20,15,00,00); q.t2 = datenum(2016,04,22,00,00,00);
+
+q.dev = 'PowerBlade';
 events = IllQuery(servAddr,DB, USER, PWD, EVENT, q);
-
 aP = -ones(numel(events),1);
+P = -ones(numel(events),1);
 tP = -ones(numel(events),1);
+for k = 1:numel(events)
+    if isfield(events{k}, 'apparent_power') && isfield(events{k}, 'power')
+        aP(k) = str2double(events{k}.apparent_power);
+        P(k) = str2double(events{k}.power);
+        tP(k) = datenum8601(events{k}.recordDate);
+    end
+end
+aP(aP==-1) = [];
+P(P==-1) = [];
+tP(tP==-1) = [];
 
+q.dev = 'BLEES';
+events = IllQuery(servAddr,DB, USER, PWD, EVENT, q);
 T = -ones(numel(events),1);
 H = -ones(numel(events),1);
 tB = -ones(numel(events),1);
 for k = 1:numel(events)
-    if isfield(events{k}, 'device')
-        if strcmp(events{k}.device,'PowerBlade')
-            if isfield(events{k}, 'apparent_power')
-                aP(k) = str2double(events{k}.apparent_power);
-                tP(k) = datenum8601(events{k}.recordDate);
-            end
-        elseif strcmp(events{k}.device,'BLEES')
-            if isfield(events{k}, 'temperature_celcius') && isfield(events{k}, 'humidity_percent')
-                T(k) = events{k}.temperature_celcius;
-                H(k) = events{k}.humidity_percent;
-                tB(k) = datenum8601(events{k}.recordDate);
-            end
-        end
+    if isfield(events{k}, 'temperature_celcius') && isfield(events{k}, 'humidity_percent')
+        T(k) = events{k}.temperature_celcius;
+        H(k) = events{k}.humidity_percent;
+        tB(k) = datenum8601(events{k}.recordDate);
     end
 end
-aP(aP==-1) = [];
-tP(tP==-1) = [];
 T(T==-1) = [];
 H(H==-1) = [];
 tB(tB==-1) = [];
 
+q.dev = 'Android';
+events = IllQuery(servAddr,DB, USER, PWD, EVENT, q);
+spl = -ones(numel(events),1);
+tA = -ones(numel(events),1);
+for k = 1:numel(events)
+    if isfield(events{k}, 'octaveFeat')
+        spl(k) = mean(events{k}.octaveFeat(:));
+        tA(k) = datenum8601(events{k}.recordDate);
+    end
+end
+spl(spl==-1) = [];
+tA(tA==-1) = [];
+
 % plot
+tStart = min([tP;tB;tA]);
 figure; 
-subplot(311);plot((tP-min(tP))*3600*24,aP,'rx-'); ylabel('Apparent Power'); xlabel('Relative time (s)');
-subplot(312);plot((tB-min(tB))*3600*24,T,'rx-'); ylabel('Temperature'); xlabel('Relative time (s)');
-subplot(313);plot((tB-min(tB))*3600*24,H,'rx-'); ylabel('Humidity'); xlabel('Relative time (s)');
+subplot(511);plot((tP-tStart)*60*24,aP,'rx-'); ylabel('Apparent Power'); xlabel('Relative time (min)');
+subplot(512);plot((tP-tStart)*60*24,P,'rx-'); ylabel('Power'); xlabel('Relative time (min)');
+subplot(513);plot((tB-tStart)*60*24,T,'rx-'); ylabel('Temperature'); xlabel('Relative time (min)');
+subplot(514);plot((tB-tStart)*60*24,H,'rx-'); ylabel('Humidity'); xlabel('Relative time (min)');
+subplot(515);plot((tA-tStart)*60*24,spl,'rx-'); ylabel('Sound Pressure Level'); xlabel('Relative time (min)');
